21b1d51e140d949dee13ac8d8d2c8fad32e4a611
Soumyadeep-Pal <soumyadeeppal97@gmail.com>
2020-11-30 20:30:19 -0700

    Adding plotting utility

--- experiments/control/utils/Collector.py
+++ experiments/control/utils/Collector.py
@@ -4,6 +4,15 @@ import numpy as np
 # makes it easy to grab arbitrary data during the execution of an agent
 # data is indexed by a key given by the user at collection time,
 # then is aggregated whenever `reset()` is called
+def tolerant_mean(arrs):
+        lens = [len(i) for i in arrs]
+        arr = np.ma.empty((np.max(lens),len(arrs)))
+        arr.mask = True
+        for idx, l in enumerate(arrs):
+            arr[:len(l),idx] = l
+        return arr.mean(axis = -1), arr.std(axis=-1)
+    
+    
 class Collector:
     def __init__(self):
         self.run_data = {}
@@ -36,5 +45,18 @@ class Collector:
         arr = np.array(arr)
         mean = np.mean(arr, axis=0)
         stderr = np.std(arr, axis=0, ddof=1) / np.sqrt(runs)
+
+        return [mean, stderr, runs]
+    
+    def getStats_list(self, name):
+        arr = self.all_data[name]
+
+        runs = len(arr)
+
+        arr = np.array(arr)
+        # mean = np.mean(arr, axis=0)
+        # stderr = np.std(arr, axis=0, ddof=1) / np.sqrt(runs)
+        
+        mean, stderr = tolerant_mean(arr)
 
         return [mean, stderr, runs]
